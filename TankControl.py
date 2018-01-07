03258be095ce050651ea8258f7b6e75a316fbd6f
Rasmus <rakoit@ttu.ee>
2018-01-07 04:34:33 +0200

    Resolved #4 Added controls and a special function

---
+++ TankControl.py
@@ -0,0 +1,32 @@
+import serial
+import time
+
+
+class TankControl:
+    def __init__(self, port):
+        self.ser = serial.Serial(port)
+        time.sleep(3)
+    def Forward(self):
+        self.ser.write(b'w')
+    def Left(self):
+        self.ser.write(b'a')
+    def Backward(self):
+        self.ser.write(b's')
+    def Right(self):
+        self.ser.write(b'd')
+    def Stop(self):
+        self.ser.write(b'f')
+    def Twerk(self):
+        for x in range(5):
+            self.Forward()
+            time.sleep(0.1)
+            self.Left()
+            time.sleep(0.1)
+            self.Backward()
+            time.sleep(0.1)
+            self.Right()
+            time.sleep(0.1)
+
+if __name__ == '__main__':
+    t = TankControl('COM6')
+    t.Twerk()
