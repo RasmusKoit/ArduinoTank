556e0a545fd3e83d8a9c45a7b3b998724274a503
Rasmus <rakoit@ttu.ee>
2018-01-08 00:51:53 +0200

    Closed #6 Optimized listener.js code to stop tank from running off when no input data comes in

--- listener.js
+++ listener.js
@@ -4,30 +4,35 @@ function buttonPressed(b) {
   }
   return b == 1.0;
 }
-
+var stopped = false;
 function gameLoop() {
   var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
   if (!gamepads) {
     return;
   }
-
+  if (!stopped){
+      fetch('/stop');
+      stopped = true;
+  }
   var gp = gamepads[0];
   if (buttonPressed(gp.buttons[12])) {
-      fetch('/forward')
+      fetch('/forward');
+      stopped = false;
   } else if (buttonPressed(gp.buttons[13])) {
-      fetch('/backward')
+      fetch('/backward');
+      stopped = false;
   } else if (buttonPressed(gp.buttons[14])) {
-      fetch('/left')
+      fetch('/left');
+      stopped = false;
   } else if (buttonPressed(gp.buttons[15])) {
-      fetch('/right')
+      fetch('/right');
+      stopped = false;
   } else if (buttonPressed(gp.buttons[1])) {
-      fetch('/twerk')
-  }
-  else {
-      fetch('/stops')
+      fetch('/twerk');
+      stopped = false;
   }
 
 
   start = requestAnimationFrame(gameLoop);
 }
-gameLoop()
+gameLoop();
